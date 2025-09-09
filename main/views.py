7da0f22481c91389fd8969df5f74c6e61298b13e
ganeshatq <ganeshataqwa01@gmail.com>
2025-09-09 20:19:01 +0700

    Revisi Tutorial 1

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406425640',
+        'name': 'Ganesha Taqwa',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
